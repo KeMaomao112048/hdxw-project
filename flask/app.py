e4d040405cd584933158da25416c910bc2830aea
KeMaomao <2412211017@stu.hqu.edu.cn>
2025-08-23 19:33:35 +0800

    反向代理

--- flask/app.py
+++ flask/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 
     
-@app.route('/hdxw')
+@app.route('/')
 def hdxw():
     db = get_connection()
     cursor = db.cursor(dictionary=True)
@@ -21,4 +21,4 @@ def hdxw():
 
 if __name__ == '__main__':
     
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
